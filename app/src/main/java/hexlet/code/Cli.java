afcdb5ed7ccdcbaef6207fdf095226c94d599779
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-04-10 22:54:10 +0300

    modify Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,8 +6,8 @@ public class Cli {
     public static String getByName() {
         System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        if (name.equals("") && name.equals(" ")) {
+        String name = scanner.nextLine().trim();
+        if (name.isEmpty()) {
             System.out.println("You didn't enter your name. Let's try again!");
             return getByName();
         }

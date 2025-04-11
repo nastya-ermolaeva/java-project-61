2d8f30fcf7ae3aa95e34992efbafa9198ca67576
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-04-11 22:16:17 +0300

    modify App.java, add Greet.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,22 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        String userName = Cli.getByName();
+        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+
+        switch (input) {
+            case "1":
+                System.out.println("Your choice: " + input);
+                Greet.greetingUser();
+                break;
+            case "0":
+                System.out.println("Your choice: " + input);
+                break;
+            default:
+                System.out.println("You entered an incorrect number.");
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Greet.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public class Greet {
+
+    public static void greetingUser() {
+        System.out.println("Welcome to the Brain Games!");
+        String userName = Cli.getByName();
+    }
+}

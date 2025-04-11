eb2900a130a28c7195ea18e9498869a5d5f5a42f
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-04-12 01:33:17 +0300

    modify Greet.java

--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -1,9 +1,21 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class Greet {
+    private static String name;
 
-    public static void greetingUser() {
-        System.out.println("Welcome to the Brain Games!");
-        String userName = Cli.getByName();
+    public static void getByName() {
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine().trim();
+        if (name.isEmpty()) {
+            System.out.println("You didn't enter your name.");
+        } else {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+
+    public static String getName() {
+        return name;
     }
 }

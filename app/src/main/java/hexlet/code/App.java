8b38af8cfc2b62cd938abb1934d79098c3be9bc2
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-04-12 01:34:48 +0300

    modify App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n 1 - Greet\n 0 - Exit");
+        System.out.println("Please enter the game number and press Enter."
+               + "\n1 - Greet"
+               + "\n2 - Even"
+               + "\n0 - Exit");
+        System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().trim();
 
         switch (input) {
             case "1":
-                System.out.println("Your choice: " + input);
-                Greet.greetingUser();
+                Greet.getByName();
                 break;
+            case "2":
+                Greet.getByName();
+                Even.isNumberEven();
             case "0":
-                System.out.println("Your choice: " + input);
                 break;
             default:
-                System.out.println("You entered an incorrect number.");
+                System.out.println("You entered an incorrect number. Let's try again!");
         }
     }
 }
